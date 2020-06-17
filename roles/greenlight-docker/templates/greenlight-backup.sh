84097bc548e064eda169da381d29cdad10accd7f
Sebastian Fischer <sebastian@fischerulm.de>
2020-06-17 13:47:25 +0200

    cleanup: removed role greenlight-docker

--- roles/greenlight-docker/templates/greenlight-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SQLDUMP=dump_greenlight_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-LOGFILE=/srv/greenlight/greenlight-backup.log
-
-{
-	docker exec ulmlernt-greenlight-postgres pg_dumpall -c -U postgres | ssh srf@lb.ulmlernt.org "cat > /var/backups/greenlight/$SQLDUMP"
-} 2>&1 | tee -a $LOGFILE
